580dc2cc6330a7ca45c8be5da1ef6d691f573d40
devendhiran0508 <devendhiran0508@gmail.com>
2025-04-13 00:04:43 +0530

    final one of login page

--- react-first-app/my-react-first-app/src/LoginPage.jsx
+++ react-first-app/my-react-first-app/src/LoginPage.jsx
@@ -1,48 +1,5 @@
 // //login page for full stack project
-// import React, { useState, useEffect } from 'react';
-// import './LoginPage.css';
-// import { FaUserAlt } from "react-icons/fa";
-// import { RiLockPasswordFill } from "react-icons/ri";
-// import { Plane } from 'lucide-react';
-
-
-
-// function LoginPage()
-// {
-//     return(
-//         <div className='wrap'>
-//             <div className='form-box'>
-//                 <form action="">
-//                     <h1>LOGIN</h1>
-//                     <div className='input-box'>
-//                         <FaUserAlt className='icon'/>
-//                         <input type="text" placeholder='USERNAME' required/>
-//                     </div>
-//                     <div className='input-box'>
-//                         <RiLockPasswordFill className='icon'/>
-//                         <input type="password" placeholder='PASSWORD' required/>
-//                     </div>
-//                     <div className='rem-for'>
-//                         <label>
-//                             <input type="checkbox" />Remember me
-//                         </label>
-//                         <a href="#">Forget Password</a>
-//                     </div>
-//                     <button type='submit'>Submit</button>
-//                     <div>
-//                         <p>Don't have an account? 
-//                             <a href="#">Register</a>
-//                         </p>
-//                     </div>
-//                 </form>
-//             </div>
-
-//         </div>
-//     )
-// }
-// export default LoginPage;
 import React, { useState, useEffect } from 'react';
-import { Plane } from 'lucide-react';
 import './LoginPage.css';
 import { FaUserAlt } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
